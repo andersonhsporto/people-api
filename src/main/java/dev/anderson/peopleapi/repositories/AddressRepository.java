df58551dfe0b3260e79ece19c1c541c10e51a9da
andersonhsporto <anderson.higo2@gmail.com>
2023-01-04 19:56:21 -0300

    add: repositories

---
+++ src/main/java/dev/anderson/peopleapi/repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package dev.anderson.peopleapi.repositories;
+
+import dev.anderson.peopleapi.entities.AddressEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+
+}
---
+++ src/main/java/dev/anderson/peopleapi/repositories/PeopleRepository.java
@@ -0,0 +1,10 @@
+package dev.anderson.peopleapi.repositories;
+
+import dev.anderson.peopleapi.entities.PeopleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<PeopleEntity, Long> {
+
+}
