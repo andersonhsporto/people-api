55aa002848c7b49e945cc6a17b22087555f50ef6
andersonhsporto <anderson.higo2@gmail.com>
2023-01-04 19:08:17 -0300

    first commit

---
+++ src/test/java/dev/anderson/peopleapi/PeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anderson.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
