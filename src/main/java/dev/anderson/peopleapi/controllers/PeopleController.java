a84254871a138cf8ba5ae29b89ffe2c2163dba3a
andersonhsporto <anderson.higo2@gmail.com>
2023-01-04 23:03:02 -0300

    add: Post method

--- src/main/java/dev/anderson/peopleapi/controllers/PeopleController.java
+++ src/main/java/dev/anderson/peopleapi/controllers/PeopleController.java
@@ -4,7 +4,9 @@ import dev.anderson.peopleapi.service.implementation.PeopleServiceImplementation
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,7 +26,13 @@ public class PeopleController {
     return ResponseEntity.ok(peopleServiceImplementation.listAll());
   }
 
-
+  @PostMapping
+  public ResponseEntity<?> makePeople(
+      @RequestParam(value = "name", required = true) String name,
+      @RequestParam(value = "birthDate", required = true) String birthDate
+  ) {
+    return peopleServiceImplementation.makePeople(name, birthDate);
+  }
 
 
 }
