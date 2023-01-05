ab8e417090069bd0a18f41bbb6b8e78f3a23da4d
andersonhsporto <anderson.higo2@gmail.com>
2023-01-04 23:02:22 -0300

    add: existsByNameAndBirthDate in repository

--- src/main/java/dev/anderson/peopleapi/repositories/PeopleRepository.java
+++ src/main/java/dev/anderson/peopleapi/repositories/PeopleRepository.java
@@ -1,10 +1,12 @@
 package dev.anderson.peopleapi.repositories;
 
 import dev.anderson.peopleapi.domain.entities.PeopleEntity;
+import java.time.LocalDate;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PeopleRepository extends PagingAndSortingRepository<PeopleEntity, Long> {
 
+  boolean existsByNameAndBirthDate(String name, LocalDate birthDate);
 }
