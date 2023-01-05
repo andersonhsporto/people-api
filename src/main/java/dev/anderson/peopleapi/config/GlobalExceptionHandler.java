e3132c747a78e0f2f6c1eb84ec65648b28fb2d43
andersonhsporto <anderson.higo2@gmail.com>
2023-01-04 23:21:56 -0300

    add: handleUserDateTimeParseException member function

--- src/main/java/dev/anderson/peopleapi/config/GlobalExceptionHandler.java
+++ src/main/java/dev/anderson/peopleapi/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.anderson.peopleapi.config;
 
 import dev.anderson.peopleapi.exceptions.UserExistsException;
+import java.time.format.DateTimeParseException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -15,11 +16,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(UserExistsException.class)
-  public ResponseEntity<Map<String, List<String>>> handleUserExistsException(UserExistsException ex) {
+  public ResponseEntity<Map<String, List<String>>> handleUserExistsException(
+      UserExistsException ex) {
     List<String> errors = Collections.singletonList(ex.getMessage());
 
-    return new ResponseEntity<>(getErrorsMap(errors),  new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+  }
 
+  @ExceptionHandler(DateTimeParseException.class)
+  public ResponseEntity<Map<String, List<String>>> handleUserDateTimeParseException(
+      DateTimeParseException ex) {
+    List<String> errors = Collections.singletonList(ex.getMessage());
+
+    return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
   }
 
   private Map<String, List<String>> getErrorsMap(List<String> errors) {
