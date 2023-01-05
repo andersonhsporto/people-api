ceb009bea1cdb8bbb6c3845cb9c407cff778d7f0
andersonhsporto <anderson.higo2@gmail.com>
2023-01-04 21:48:46 -0300

    refactor: using PagingAndSortingRepository

--- src/main/java/dev/anderson/peopleapi/repositories/PeopleRepository.java
+++ src/main/java/dev/anderson/peopleapi/repositories/PeopleRepository.java
@@ -1,10 +1,10 @@
 package dev.anderson.peopleapi.repositories;
 
 import dev.anderson.peopleapi.domain.entities.PeopleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PeopleRepository extends JpaRepository<PeopleEntity, Long> {
+public interface PeopleRepository extends PagingAndSortingRepository<PeopleEntity, Long> {
 
 }
