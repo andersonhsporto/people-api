8014cc984428362086eaa17e5c31b7ae0ef90e01
andersonhsporto <anderson.higo2@gmail.com>
2023-01-05 01:02:38 -0300

    refactor: removal if from listAll member function

--- src/main/java/dev/anderson/peopleapi/service/implementation/PeopleServiceImplementation.java
+++ src/main/java/dev/anderson/peopleapi/service/implementation/PeopleServiceImplementation.java
@@ -1,11 +1,13 @@
 package dev.anderson.peopleapi.service.implementation;
 
+import dev.anderson.peopleapi.domain.DTO.PeopleDTO;
 import dev.anderson.peopleapi.domain.entities.PeopleEntity;
 import dev.anderson.peopleapi.exceptions.UserExistsException;
 import dev.anderson.peopleapi.repositories.PeopleRepository;
 import dev.anderson.peopleapi.service.PeopleService;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,18 @@ public class PeopleServiceImplementation implements PeopleService {
 
   @Override
   public ResponseEntity<?> listAll() {
-    return new ResponseEntity<>("Hello World", null, 200);
+    List<PeopleEntity> peopleList = peopleRepository.findAll();
+
+    return new ResponseEntity<>(PeopleDTO.fromList(peopleList), null, 200);
   }
+
+//  @Override
+//  public ResponseEntity<?> listAll() {
+//    PeopleEntity peopleEntity = peopleRepository.getReferenceById(1L);
+//
+//    return new ResponseEntity<>(PeopleDTO.of(peopleEntity.getName(), "TESTE"),
+//        null, 200);
+//  }
 
   @Override
   public ResponseEntity<?> makePeople(String name, String birthDate) {
@@ -36,7 +48,6 @@ public class PeopleServiceImplementation implements PeopleService {
     }
     return new ResponseEntity<>(null, null, 200);
   }
-
 
   LocalDate parseDate(String date) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
