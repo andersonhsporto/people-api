67cb572824a7f03f49bf9bff28f0052915f12252
andersonhsporto <anderson.higo2@gmail.com>
2023-01-05 01:01:02 -0300

    add: static factory member function

--- src/main/java/dev/anderson/peopleapi/domain/DTO/PeopleDTO.java
+++ src/main/java/dev/anderson/peopleapi/domain/DTO/PeopleDTO.java
@@ -1,8 +1,29 @@
 package dev.anderson.peopleapi.domain.DTO;
 
-public record PeopleDTO(String name, String birthDate) {
+import dev.anderson.peopleapi.domain.entities.PeopleEntity;
+import java.util.ArrayList;
+import java.util.List;
 
-  public static PeopleDTO of(String name, String birthDate) {
-    return new PeopleDTO(name, birthDate);
+public record PeopleDTO(
+    String name,
+    String birthDate,
+    List<AddressDTO> addressDTO) {
+
+  public static PeopleDTO of(PeopleEntity peopleEntity) {
+    return new PeopleDTO(
+        peopleEntity.getName(),
+        peopleEntity.getBirthDate().toString(),
+        AddressDTO.fromEntityList(peopleEntity.getAddresses())
+    );
   }
+
+  public static List<PeopleDTO> fromList(List<PeopleEntity> peopleEntityList) {
+    List<PeopleDTO> peopleDTOList = new ArrayList<>();
+
+    for (PeopleEntity peopleEntity : peopleEntityList) {
+      peopleDTOList.add(of(peopleEntity));
+    }
+    return peopleDTOList;
+  }
+
 }
