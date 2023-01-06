cc3145b52be84d0a500bcf1408cc98c8d9d47dd3
andersonhsporto <anderson.higo2@gmail.com>
2023-01-06 01:10:34 -0300

    add: member functions in interface

--- src/main/java/dev/anderson/peopleapi/service/AddressService.java
+++ src/main/java/dev/anderson/peopleapi/service/AddressService.java
@@ -1,8 +1,13 @@
 package dev.anderson.peopleapi.service;
 
+import dev.anderson.peopleapi.domain.DTO.AddressDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface AddressService {
 
   ResponseEntity<?> listAll();
+
+  ResponseEntity<?> findAddress(String name, String birthDate);
+
+  ResponseEntity<?> makeAddress(String name, String birthDate, AddressDTO addressDTO);
 }
