b91bde98d8048626ade61f153e5ce4b7408458bb
andersonhsporto <anderson.higo2@gmail.com>
2023-01-07 01:23:31 -0300

    add: formattedDate member function fix

--- src/main/java/dev/anderson/peopleapi/domain/DTO/PeopleDTO.java
+++ src/main/java/dev/anderson/peopleapi/domain/DTO/PeopleDTO.java
@@ -1,6 +1,8 @@
 package dev.anderson.peopleapi.domain.DTO;
 
 import dev.anderson.peopleapi.domain.entities.PeopleEntity;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.domain.Page;
@@ -13,7 +15,7 @@ public record PeopleDTO(
   public static PeopleDTO of(PeopleEntity peopleEntity) {
     return new PeopleDTO(
         peopleEntity.getName(),
-        peopleEntity.getBirthDate().toString(),
+        formattedDate(peopleEntity.getBirthDate()),
         AddressDTO.fromEntityList(peopleEntity.getAddresses())
     );
   }
@@ -25,6 +27,10 @@ public record PeopleDTO(
       peopleDTOList.add(of(peopleEntity));
     }
     return peopleDTOList;
+  }
+
+  private static String formattedDate(LocalDate date) {
+    return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).replace("-", "/");
   }
 
 }
