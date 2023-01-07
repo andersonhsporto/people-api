36ad8c2dfa501c9d62ea9a75702fa63cd5f4cb9d
andersonhsporto <anderson.higo2@gmail.com>
2023-01-06 23:37:14 -0300

    fix: openApi member function name

--- src/main/java/dev/anderson/peopleapi/config/OpenApi.java
+++ src/main/java/dev/anderson/peopleapi/config/OpenApi.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApi {
 
   @Bean
-  public OpenAPI springShopOpenAPI() {
+  public OpenAPI springPeopleAPI() {
     return new OpenAPI()
         .info(new Info().title("People API")
             .description("People management API")
